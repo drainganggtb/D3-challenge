2a847a73bb7a53418c7a797a2da3b666511918f2
Justin <gree2167@umn.edu>
2021-01-21 18:10:38 -0600

    working on first chart

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,35 @@
+
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//import data from csv
+d3.csv("assets/data/data.csv").then(function(journalData) {
+    journalData.forEach(function(data) {
+        data.
+    })
+})
